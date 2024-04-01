78dc92de1008aede90f6bc701755017ac8daf4e7
jdmartinev <110254820+jdmartinev@users.noreply.github.com>
2024-04-01 12:11:16 -0500

    Update add_images_db.py

--- aux_files/add_images_db.py
+++ aux_files/add_images_db.py
@@ -2,15 +2,18 @@ from django.core.management.base import BaseCommand
 from movie.models import Movie
 import json
 import os
-import openai
+from openai import OpenAI
 import requests
 from PIL import Image
 from io import BytesIO
 
 from dotenv import load_dotenv, find_dotenv
 
-_ = load_dotenv('../openAI.env')
-openai.api_key  = os.environ['openAI_api_key']
+_ = load_dotenv('openAI.env')
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get('openAI_api_key'),
+)
 
 def fetch_image(url):
     response = requests.get(url)
@@ -26,15 +29,16 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         items = Movie.objects.all()
         for item in items:
-            response = openai.Image.create(
+            response = client.images.generate(
+                model="dall-e-2",
                 prompt=f"Alguna escena de la película {item.title}",
+                size="256x256",
+                quality="standard",
                 n=1,
-                size="256x256"
             )
-            image_url = response['data'][0]['url']
+            image_url = response['data'][0].url
             img = fetch_image(image_url)
             img.save(f'media/movie/images/{item.title}.jpg')           
             item.image = f'movie/images/{item.title}.jpg'   
             item.save()
-        self.stdout.write(self.style.SUCCESS(f'Successfully updated item with ID {item_id}'))
-        
+        self.stdout.write(self.style.SUCCESS(f'Successfully updated items'))
